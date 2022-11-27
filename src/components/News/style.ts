00e5165d7a25e4636b48562ec96d8617300fe6f5
davivsouza <davivasconcelossouza7@gmail.com>
2022-11-27 12:06:01 -0300

    melhorando o visual para desktop

--- src/components/News/style.ts
+++ src/components/News/style.ts
@@ -3,34 +3,59 @@ import styled from "styled-components";
 export const Container = styled.main`
   min-height: 100vh;
   width: 100%;
+  
 
   background: linear-gradient(88.59deg, #3CAC59 -66%, #32C457 98.8%);
 
   padding: 350px 40px 20px;
+
+  @media (min-width: 640px) {
+    padding: 120px 40px 20px;
+    }
   position: relative;
   h1, p{
     font-family: 'Sen';
     color: #fff;
     margin-bottom: 8px;
+
+    @media (min-width: 1024px) {
+      max-width: 500px;
+      margin: auto;
+    }
   }
+
+  
   .back{
     position:absolute ;
     top: 20px;
     left: 20px;
     z-index: 2;
+
+    @media (min-width: 1024px) {
+      top: 90px;
+      left: 120px;
+    }
   }
   .wallpaper{
-    position:absolute ;
+    position: absolute ;
     top: 0;
     left: 0;
     width: 100%;
     height: 300px;
     z-index: 1;
+
+    @media (min-width: 640px) {
+      display: none;
+    }
   }
 
   .news-content{
+    display:flex ;
+    flex-direction: column;
+    align-items: center;
+   
     margin-top: 16px;
-    p{
+    p, strong{
       font-family:'Sen' ;
       color: #fff;
       text-align: justify;
@@ -40,6 +65,11 @@ export const Container = styled.main`
       width:100% ;
       margin: 24px 0;
       border-radius: 15px;
+
+      @media (min-width: 1024px) {
+        width: 40%;
+        margin: 25px auto;
+      }
     }
   }
 `
--- src/styles/container-pages.ts
+++ src/styles/container-pages.ts
@@ -2,9 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   min-height: 100vh;
+    
+  
   width: 100%;
 
   background: linear-gradient(88.59deg, #3CAC59 -66%, #32C457 98.8%);
 
   padding: 20px 40px;
+  main{
+    @media (min-width: 1024px) {
+      max-width: 900px;
+      margin: auto;
+    }
+  }
+  
 `;
